58c049e399f2cf40b234cc12f60887a451eecb13
Caleb Harden <cbh32890@gmail.com>
2025-09-02 12:40:53 -0500

    Fix setup.py to remove deprecated pip.req import

--- setup.py
+++ setup.py
@@ -2,20 +2,32 @@
 
 import os
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
+# Function to parse requirements.txt
+def parse_requirements(filename):
+    requirements = []
+    with open(filename, 'r') as file:
+        for line in file:
+            line = line.strip()
+            # Skip empty lines, comments, or git URLs
+            if line and not line.startswith('#') and not line.startswith('git+'):
+                requirements.append(line)
+    return requirements
+
+# Get the directory of setup.py
 setup_dir = os.path.dirname(os.path.realpath(__file__))
 path_req = os.path.join(setup_dir, 'requirements.txt')
-install_reqs = parse_requirements(path_req, session=False)
 
-reqs = [str(ir.req) for ir in install_reqs]
+# Parse requirements
+reqs = parse_requirements(path_req)
 
-setup(name='pgoapi',
-      author = 'tjado',
-      description = 'Pokemon Go API lib',
-      version = '2.14.0',
-      url = 'https://github.com/goedzo/pgoapi',
-      download_url = "https://github.com/pogodevorg/pgoapi/releases",
-      packages = find_packages(),
-      install_requires = reqs
-      )
+setup(
+    name='pgoapi',
+    author='tjado',
+    description='Pokemon Go API lib',
+    version='2.14.0',
+    url='https://github.com/goedzo/pgoapi',
+    download_url='https://github.com/pogodevorg/pgoapi/releases',
+    packages=find_packages(),
+    install_requires=reqs
+)
